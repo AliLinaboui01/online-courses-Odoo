0c21d94ca941d0daaf83e9bfe56ef0a61cd2192b
Hamzaakk <akkaouih17@gmail.com>
2024-12-24 11:38:40 +0100

    init foder structure

--- __manifest__.py
+++ __manifest__.py
@@ -20,26 +20,8 @@
     'depends': ['base',"web","website"],
 
     'data': [
-        'security/access_rights.csv',
-        'views/course_management_view.xml',
-        'views/user_profiles_view.xml',
-        'views/lesson_details_view.xml',
-        'views/course_attachments_view.xml',
-        'views/homepage_layout.xml',
-        'views/course_overview.xml',
-        'views/course_ratings_view.xml',
-        'views/browse_courses_view.xml'
+       
     ],
     
-    'demo': [
-        'demonstration.xml',
-    ],
-    'assets': {
-            'web.assets_frontend': [
-                            'https://code.jquery.com/jquery-3.5.1.slim.min.js',
-                            'https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.8/dist/umd/popper.min.js',
-                            'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
-                            '/e_courses/static/lib/pdfslidesviewer/PDFSlidesViewer.js'
-            ]
-        }
+   
 }
