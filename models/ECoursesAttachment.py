79ca17ae7b3d3e2e4462d605c979698e39666325
Hamzaakk <akkaouih17@gmail.com>
2024-12-24 11:54:17 +0100

    add some fildes to attachment model

--- models/ECoursesAttachment.py
+++ models/ECoursesAttachment.py
@@ -5,4 +5,10 @@ class ECoursesAttachment(models.Model):
     _name = 'e_courses.attachment'
     _description = 'Lesson Attachments'
 
-    # ... (fields and methods remain unchanged)
+    file = fields.Binary(string='File')
+    lesson_id = fields.Many2one('e_courses.lesson', string='Lesson', required=True)
+
+    @api.model
+    def create(self, values):
+        attachment = super(Attachment, self).create(values)
+        return attachment
